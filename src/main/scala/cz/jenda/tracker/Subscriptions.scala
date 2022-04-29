833d1e1c000a0e6b7638259892eaa8c27373b073
Jenda Kolena <jendakolena@gmail.com>
2022-04-30 00:14:18 +0200

    Realtime map updating

---
+++ src/main/scala/cz/jenda/tracker/Subscriptions.scala
@@ -0,0 +1,34 @@
+package cz.jenda.tracker
+
+import cats.effect.concurrent.Ref
+import cz.jenda.tracker.Subscription.ForCoordinates
+import monix.eval.Task
+import org.typelevel.log4cats.slf4j.Slf4jLogger
+
+class Subscriptions {
+  private val logger = Slf4jLogger.getLoggerFromClass[Task](classOf[Subscriptions])
+
+  private val subscriptions: Ref[Task, List[Subscription]] = Ref.unsafe(Nil)
+
+  def subscribe(s: Subscription): Task[Unit] = {
+    subscriptions.modify(l => (s :: l, ()))
+  }
+
+  def newCoordinates(c: Coordinates): Task[Unit] = {
+    subscriptions.get
+      .map(_.collect { case ForCoordinates(trackId, enqueue) if trackId == c.trackId => enqueue })
+      .flatMap { l =>
+        Task
+          .parSequenceUnordered(l.map {
+            _.apply(c).onErrorHandleWith(e => logger.warn(e)("Could not send subscription update"))
+          })
+          .void
+      }
+  }
+}
+
+sealed trait Subscription
+
+object Subscription {
+  case class ForCoordinates(trackId: Int, enqueue: Coordinates => Task[Unit]) extends Subscription
+}
