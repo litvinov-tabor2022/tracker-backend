bdc8bd3be2dc35f3bb15ae6a910b511e4f85896a
Jenda Kolena <jendakolena@gmail.com>
2022-05-05 22:44:21 +0200

    Improve deploy script

--- build-and-deploy.sh
+++ build-and-deploy.sh
@@ -6,5 +6,5 @@ sbt -Dversion="$1" docker:publishLocal
 
 git tag "$1"
 
-docker save tracker-server:latest | pv -s 200000000 | ssh cloud 'docker load'
-ssh cloud './docker/tracker-server_recreate.sh'
+docker save "tracker-server:$1" | pv -s 200000000 | ssh cloud 'docker load'
+ssh cloud "./docker/tracker-server_recreate.sh $1"
